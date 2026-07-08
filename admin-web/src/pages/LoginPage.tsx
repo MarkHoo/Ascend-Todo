c2cd29ae9f957356be733b117614d53fb0271989
MarkHoo <markhoo911@gmail.com>
2026-07-08 13:57:46 +0800

    chore: bump version to v2.2.5

--- admin-web/src/pages/LoginPage.tsx
+++ admin-web/src/pages/LoginPage.tsx
@@ -17,7 +17,7 @@ export function LoginPage() {
         deviceName: 'Admin Web',
         deviceFingerprint: `admin-web-${navigator.userAgent}`,
         platform: navigator.platform,
-        appVersion: '2.2.4',
+        appVersion: '2.2.5',
       });
       setToken(result.accessToken);
       navigate('/');
